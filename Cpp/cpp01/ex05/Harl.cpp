c5bb79e14a2976f1e0b58b7e1eefb01a39aeb888
Mathis ROLY <maroly@e3r12p13.clusters.42paris.fr>
2022-05-05 12:48:23 +0200

    transfert encore

---
+++ Cpp/cpp01/ex05/Harl.cpp
@@ -0,0 +1,29 @@
+#include "Harl.hpp"
+
+Harl::Harl(void){
+    return ;
+}
+
+Harl::~Harl(void){
+    return ;
+}
+
+void Harl::debug(){
+
+}
+
+void Harl::info(){
+
+}
+
+void Harl::warning(){
+    
+}
+
+void Harl::error(){
+
+}
+
+void Harl::complain(std::string level){
+    std::invoke()
+}
